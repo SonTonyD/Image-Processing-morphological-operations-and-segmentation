858bf1bc85f12a84dcfbe11a4e97ae7016438a2a
SonTonyD <sontony.dinh@gmail.com>
2022-12-06 12:23:49 +0100

    M5_Variant recursive function OK

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from PIL import Image
 import numpy as np
 import click
+import time
 
 import module.morphologicalOperation as mop
 import module.basicSetOperation as bso
+import module.segmentationOperation as seo
 
 def saveResult(image_matrix, save_path):
     Image.fromarray(image_matrix).save(save_path)
@@ -21,8 +23,6 @@ def operation(name) :
     SE2 = np.array([[False,False,False],[False,True,False],[False,True,False]])
     SE3 = np.array([[True,True,True],[True,True,True],[True,True,True]])
 
-    SE_hit = np.array([[True,True,True],[False,False,False],[False,False,False]])
-    SE_miss = np.array([[False,False,False],[True,False,True],[True,True,True]])
     
     
     result = mop.dilation(image_matrix, SE1)
@@ -30,7 +30,27 @@ def operation(name) :
     #result = mop.opening(image_matrix, SE1)
     #result = mop.closing(image_matrix, SE1)
 
-    #result = mop.hitOrMiss(image_matrix, SE_hit, SE_miss)
+    #result = mop.HMT(image_matrix, SE_hit, SE_miss)
+
+    '''
+    index must be 7
+    super_index is the number of M5_operation you want
+    '''
+    numberOfM5 = 1
+    SE_INDEX_CONST = 7
+    start = time.perf_counter()
+
+    #result = mop.super_M5_variant(image_matrix, mop.SE_xii, SE_INDEX_CONST, numberOfM5)
+    tmp = mop.M5_variant(image_matrix, mop.SE_xii, SE_INDEX_CONST)
+    result = mop.M5_variant(tmp, mop.SE_xii, SE_INDEX_CONST)
+
+    end = time.perf_counter()
+    print(f"M5 Operation:  {end - start:0.4f} seconds")
+    print("for",numberOfM5," repeats")
+
+
+
+    
 
     Image.fromarray(result).show("New Image")
 
--- module/morphologicalOperation.py
+++ module/morphologicalOperation.py
@@ -3,6 +3,42 @@ import numpy as np
 
 import module.basicSetOperation as bso
 
+#xii Structural Element
+B1_hit = np.array([[False,False,False],[False,True,False],[True,True,True]])
+B1_miss = np.array([[False,False,False],[True,False,True],[False,False,False]])
+B1 = np.array([B1_hit,B1_miss])
+
+B2_hit = np.array([[False,False,False],[True,True,False],[True,True,False]])
+B2_miss = np.array([[True,False,False],[False,False,False],[False,False,True]])
+B2 = np.array([B2_hit,B2_miss])
+
+B3_hit = np.array([[True,False,False],[True,True,False],[True,False,False]])
+B3_miss = np.array([[False,True,False],[False,False,False],[False,True,False]])
+B3 = np.array([B3_hit,B3_miss])
+
+B4_hit = np.array([[True,True,False],[True,True,False],[False,False,False]])
+B4_miss = np.array([[False,False,True],[False,False,False],[True,False,False]])
+B4 = np.array([B4_hit,B4_miss])
+
+B5_hit = np.array([[True,True,True],[False,True,False],[False,False,False]])
+B5_miss = np.array([[False,False,False],[True,False,True],[False,False,False]])
+B5 = np.array([B5_hit,B5_miss])
+
+B6_hit = np.array([[False,True,True],[False,True,True],[False,False,False]])
+B6_miss = np.array([[True,False,False],[False,False,False],[False,False,True]])
+B6 = np.array([B6_hit,B6_miss])
+
+B7_hit = np.array([[False,False,True],[False,True,True],[False,False,True]])
+B7_miss = np.array([[False,True,False],[False,False,False],[False,True,False]])
+B7 = np.array([B7_hit,B7_miss])
+
+B8_hit = np.array([[False,False,False],[False,True,True],[False,True,True]])
+B8_miss = np.array([[False,False,True],[False,False,False],[True,False,False]])
+B8 = np.array([B8_hit,B8_miss])
+
+SE_xii = np.array([B1,B2,B3,B4,B5,B6,B7,B8])
+
+
 def dilation(image_matrix, SE):
     width, height = image_matrix.shape[0], image_matrix.shape[1]
     SEwidth, SEheight = SE.shape[0], SE.shape[1]
@@ -43,8 +79,26 @@ def opening(image_matrix, SE):
 def closing(image_matrix, SE):
     return erosion(dilation(image_matrix, SE), SE) 
 
-def hitOrMiss(image_matrix, SE_hit, SE_miss):
+def HMT(image_matrix, SE_hit, SE_miss):
     element_1 = erosion(image_matrix, SE_hit)
     element_2 = erosion(bso.complement(image_matrix), SE_miss)
     return bso.intersection(element_1, element_2)
+
+
+
+def super_M5_variant(image_matrix, SE_xii, index, super_index):
+    if super_index == 0:
+        return M5_variant(image_matrix, SE_xii, index)
+    return super_M5_variant(M5_variant(image_matrix, SE_xii, index), SE_xii, index, super_index-1)
+
+def M5_variant(image_matrix, SE_xii, index):
+    if index == 0:
+        return N_operation(image_matrix, SE_xii[index])
+    return M5_variant(N_operation(image_matrix, SE_xii[index]), SE_xii, index-1)
+        
+
+def N_operation(image_matrix, SE_HMT):
+    element_1 = bso.complement(HMT(image_matrix, SE_HMT[0], SE_HMT[1]))
+    return bso.intersection(image_matrix, element_1)
+
 
